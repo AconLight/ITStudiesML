84d1669e6e3128848aab3b1ff7a79b135ae4c4ee
Hyphen133 <hyphen133@gmail.com>
2020-03-11 22:48:25 +0100

    Added case for enum entry

--- src/dataLoading/dataLoader.py
+++ src/dataLoading/dataLoader.py
@@ -3,9 +3,9 @@ from src.common.configuration.conf import DataLoadingConfigurationEntries
 
 class DataLoader:
     def __init__(self, configuration):
-        self.feedColumns = configuration.get_entry(DataLoadingConfigurationEntries.FEED_COLUMNS.value)
-        self.classificationColumn = configuration.get_entry(DataLoadingConfigurationEntries.CLASSIFICATION_COLUMN.value)
-        self.data_file_path = configuration.get_entry(DataLoadingConfigurationEntries.DATA_FILEPATH.value)
+        self.feedColumns = configuration.get_entry(DataLoadingConfigurationEntries.FEED_COLUMNS)
+        self.classificationColumn = configuration.get_entry(DataLoadingConfigurationEntries.CLASSIFICATION_COLUMN)
+        self.data_file_path = configuration.get_entry(DataLoadingConfigurationEntries.DATA_FILEPATH)
 
 
     def load(self, encoding="ISO-8859-1"):
