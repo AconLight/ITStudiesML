0f14562fc9f0eb6dd0f5f4be54b91264440555f4
Hyphen133 <hyphen133@gmail.com>
2020-03-14 20:21:53 +0100

    Fixed bug wrong type

--- src/modelProcessing/modelProcessor.py
+++ src/modelProcessing/modelProcessor.py
@@ -1,11 +1,14 @@
 import importlib
 
+from pandas import DataFrame
+
+
 class ModelProcessor:
     def __init__(self, configuration):
         self.model = getattr(importlib.import_module("src.modelProcessing.modelAlgorithms.algorithms"), configuration.get_entry('modelAlgorithm'))()
 
     def process(self, X_train, X_test, Y_train):
         self.model.train(X_train, Y_train)
-        return self.model.predict(X_test)
+        return DataFrame(self.model.predict(X_test), columns=Y_train.columns)
 
 
