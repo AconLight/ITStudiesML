1c55d59895baa52e8726186431eff3dac9c3a511
stanislaw-zakrzewski <stanislaw.zakrzewski13@gmail.com>
2020-03-15 17:50:29 +0100

    naive byes

--- src/modelProcessing/modelAlgorithms/algorithms/naive_bayes.py
+++ src/modelProcessing/modelAlgorithms/algorithms/naive_bayes.py
@@ -1,18 +1,14 @@
-from scipy.stats import norm
-import numpy as np
-import pandas as pd
 from sklearn.naive_bayes import GaussianNB
 
 
-class NaiveBayes():
+class NaiveBayes:
     def __init__(self, conf) -> None:
         super().__init__()
-        self.classifier = GaussianNB()
+        var_smoothing = float(conf.get_entry('var_smoothing'))
+        self.classifier = GaussianNB(var_smoothing=var_smoothing)
 
-    def train(self,X_train, Y_train):
-        self.classifier.fit(X_train,Y_train)
+    def train(self, x_train, y_train):
+        self.classifier.fit(x_train, y_train)
 
-    def predict(self,X_test):
-        return self.classifier.predict(X_test)
-
-
+    def predict(self, x_test):
+        return self.classifier.predict(x_test)
