278df1b9553a54f720d172ac4c37c4f1287edb01
wmosienski <w.mosienski@gmail.com>
2020-03-15 12:51:26 -0400

    desiciontree clean up, svm parameters

--- src/modelProcessing/modelAlgorithms/algorithms/decisionTree.py
+++ src/modelProcessing/modelAlgorithms/algorithms/decisionTree.py
@@ -3,13 +3,13 @@ from sklearn.tree import DecisionTreeClassifier
 class DecisionTree():
     def __init__(self, conf) -> None:
         super().__init__()
-        criterion = conf.get_entry('criterion')
-        splitter = conf.get_entry('splitter')
-        max_depth = int(conf.get_entry('max_depth'))
-        min_samples_split = int(conf.get_entry('min_samples_split'))
-        min_samples_leaf = int(conf.get_entry('min_samples_leaf'))
-        self.classifier = DecisionTreeClassifier(criterion, splitter, max_depth, min_samples_split,
-                                                      min_samples_leaf)
+        self.classifier = DecisionTreeClassifier(
+            criterion=conf.get_entry('criterion'),
+            splitter=conf.get_entry('splitter'),
+            max_depth=int(conf.get_entry('max_depth')),
+            min_samples_split=int(conf.get_entry('min_samples_split')),
+            min_samples_leaf=int(conf.get_entry('min_samples_leaf'))
+        )
 
     def train(self,X_train, Y_train):
         self.classifier.fit(X_train, Y_train)
--- src/modelProcessing/modelAlgorithms/algorithms/svm.py
+++ src/modelProcessing/modelAlgorithms/algorithms/svm.py
@@ -2,7 +2,10 @@ from sklearn.svm import SVC
 
 class SVM:
     def __init__(self, conf):
-        self.clf = SVC(kernel='linear')
+        self.clf = SVC(
+            kernel=conf.get_entry('kernel'), # ‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed’
+            tol=float(conf.get_entry('tol')) # tolerance error
+        )
 
     def train(self, X_train, Y_train):
         self.clf.fit(X_train, Y_train)
