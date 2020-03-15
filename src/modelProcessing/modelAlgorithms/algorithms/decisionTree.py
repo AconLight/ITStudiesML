5340deb950cbea3a491654c072904f309e0dd809
wmosienski <w.mosienski@gmail.com>
2020-03-15 08:00:05 -0400

    karolinka

--- src/modelProcessing/modelAlgorithms/algorithms/decisionTree.py
+++ src/modelProcessing/modelAlgorithms/algorithms/decisionTree.py
@@ -5,11 +5,11 @@ from sklearn.datasets import load_iris
 class DecisionTree():
     def __init__(self) -> None:
         super().__init__()
-        # criterion = "gini"
-        # splitter = "best"
-        # max_depth = 2
-        # min_samples_split = 2
-        # min_samples_leaf = 1
+        criterion = "gini"
+        splitter = "best"
+        max_depth = 2
+        min_samples_split = 2
+        min_samples_leaf = 1
         self.classifier = tree.DecisionTreeClassifier(criterion, splitter, max_depth, min_samples_split,
                                                       min_samples_leaf)
 
