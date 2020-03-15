119ecf852da8c34a5ad3c471e3da54d2c7eaddc5
stanislaw-zakrzewski <stanislaw.zakrzewski13@gmail.com>
2020-03-15 18:11:44 +0100

    KNN

--- src/modelProcessing/modelAlgorithms/algorithms/decisionTree.py
+++ src/modelProcessing/modelAlgorithms/algorithms/decisionTree.py
@@ -1,18 +1,20 @@
 from sklearn.tree import DecisionTreeClassifier
 
-class DecisionTree():
+
+class DecisionTree:
     def __init__(self, conf) -> None:
         super().__init__()
-        criterion = conf.get_entry('criterion')
-        splitter = conf.get_entry('splitter')
+        criterion = conf.get_entry('criterion')  # {“gini”, “entropy”}
+        splitter = conf.get_entry('splitter')  # {“best”, “random”}
         max_depth = int(conf.get_entry('max_depth'))
         min_samples_split = int(conf.get_entry('min_samples_split'))
         min_samples_leaf = int(conf.get_entry('min_samples_leaf'))
-        self.classifier = DecisionTreeClassifier(criterion, splitter, max_depth, min_samples_split,
-                                                      min_samples_leaf)
+        self.classifier = DecisionTreeClassifier(criterion=criterion, splitter=splitter, max_depth=max_depth,
+                                                 min_samples_split=min_samples_split,
+                                                 min_samples_leaf=min_samples_leaf)
 
-    def train(self,X_train, Y_train):
-        self.classifier.fit(X_train, Y_train)
+    def train(self, x_train, y_train):
+        self.classifier.fit(x_train, y_train)
 
-    def predict(self,X_test):
-        return self.classifier.predict(X_test)
+    def predict(self, x_test):
+        return self.classifier.predict(x_test)
