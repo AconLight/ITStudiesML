8f154403295be7211cef8ed456544d9adadf0827
wmosienski <w.mosienski@gmail.com>
2020-03-15 13:09:15 -0400

    random forest params

--- src/modelProcessing/modelAlgorithms/algorithms/random_forest.py
+++ src/modelProcessing/modelAlgorithms/algorithms/random_forest.py
@@ -4,7 +4,11 @@ from sklearn.ensemble import RandomForestClassifier
 class RandomForest:
     def __init__(self, conf) -> None:
         super().__init__()
-        self.classifier = RandomForestClassifier()
+        self.classifier = RandomForestClassifier(
+            n_estimators=int(conf.get_entry('n_estimators')),
+            max_depth=int(conf.get_entry('max_depth')),
+            max_leaf_nodes=int(conf.get_entry('max_leaf_nodes')),
+        )
 
     def train(self, x_train, y_train):
         self.classifier.fit(x_train, y_train)
