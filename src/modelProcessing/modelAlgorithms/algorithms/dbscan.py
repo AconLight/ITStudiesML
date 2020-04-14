ef860d75739676caf157bb35075b8ed13e181903
wmosienski <w.mosienski@gmail.com>
2020-04-14 17:18:11 -0400

    tag2

--- src/modelProcessing/modelAlgorithms/algorithms/dbscan.py
+++ src/modelProcessing/modelAlgorithms/algorithms/dbscan.py
@@ -7,11 +7,11 @@ class DBSCAN(AlgorithmBaseGroup):
     def __init__(self, conf):
         super().__init__(conf)
         self.min_samples = int(conf.get_entry('min_samples'))
-        self.leaf_size = int(conf.get_entry('leaf_size'))
+        self.eps = float(conf.get_entry('eps'))
         self.algorithm = conf.get_entry('algorithm')
 
     def train(self):
-        dbscan = sklearn.cluster.DBSCAN(min_samples=self.min_samples, leaf_size=self.leaf_size, algorithm=self.algorithm)
+        dbscan = sklearn.cluster.DBSCAN(min_samples=self.min_samples, eps=self.eps, algorithm=self.algorithm)
         self.dbpred = None
         self.dbpred = dbscan.fit_predict(self.X)
 
