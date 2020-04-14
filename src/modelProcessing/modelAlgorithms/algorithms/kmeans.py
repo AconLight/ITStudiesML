4493e7fa6c27379ca944760491f0eaf41a617637
wmosienski <w.mosienski@gmail.com>
2020-04-14 17:29:14 -0400

    real tag 2

--- src/modelProcessing/modelAlgorithms/algorithms/kmeans.py
+++ src/modelProcessing/modelAlgorithms/algorithms/kmeans.py
@@ -6,10 +6,9 @@ from src.modelProcessing.modelAlgorithms.algorithmBaseGroup import AlgorithmBase
 class KMeans(AlgorithmBaseGroup):
     def __init__(self, conf):
         super().__init__(conf)
-        clusters = conf.get_entry('clusters')  # {“gini”, “entropy”}
         n_clusters = int(conf.get_entry('n_clusters'))
         init = conf.get_entry('init')
-        max_iter = conf.get_entry('max_iter')
+        max_iter = int(conf.get_entry('max_iter'))
         self.kmeans = sklearn.cluster.KMeans(n_clusters=n_clusters, init=init, max_iter=max_iter)
 
     def train(self):
