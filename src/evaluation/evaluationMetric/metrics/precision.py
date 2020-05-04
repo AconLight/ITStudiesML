e3c639c411e864ef42be8a570591fafd0b6418d8
wmosienski <w.mosienski@gmail.com>
2020-05-04 14:38:14 -0400

    asd

--- src/evaluation/evaluationMetric/metrics/precision.py
+++ src/evaluation/evaluationMetric/metrics/precision.py
@@ -5,4 +5,4 @@ class PrecisionMetric:
 
     @staticmethod
     def calculate(y_prediction, data_map):
-        return precision_score(data_map['FEED_COLUMNS'], y_prediction)
+        return precision_score(data_map['CLASSIFICATION_COLUMN_test'], y_prediction)
