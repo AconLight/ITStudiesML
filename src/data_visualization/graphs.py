bbee08d17c54051353cbc7a27830156518669933
Stanisław Zakrzewski <stanislaw.zakrzewski13@gmail.com>
2020-05-04 18:07:17 +0200

    plot title

--- src/data_visualization/graphs.py
+++ src/data_visualization/graphs.py
@@ -28,12 +28,12 @@ def parameter_comparison_plot(algorithm_id, database_id, metric_id, parameter_id
     plt.cla()
     plt.clf()
 
+
 def groups_dist_plot(algorithm_id, database_id, metric_id, parameter_id, parameter_values):
     # for columns
     for index, parameter_value in enumerate(parameter_values):
         print(parameter_value['data'])
         print(parameter_value['process_result'])
-
 
 
 def algorithm_comparison_plot(database_id, metric_id, best_results):
@@ -57,7 +57,9 @@ def parameter_plot(database_id, metric_id, algorithm_id, parameter_id, column_1_
 
     sns.scatterplot(x=column_1_id, y=column_2_id, hue='classes', data=df)
 
-    file_path = get_file_path(['classes', database_id[5:-4],  metric_id, algorithm_id, parameter_id, column_1_id, column_2_id])
+    file_path = get_file_path(
+        ['classes', database_id[5:-4], metric_id, algorithm_id, parameter_id, column_1_id, column_2_id])
+    plt.title("Plot {} {} {} {}".format(database_id[5:-4], metric_id, algorithm_id, parameter_id))
     plt.savefig(file_path, format='png')
     plt.cla()
     plt.clf()
