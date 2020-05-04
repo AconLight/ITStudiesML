ae3727f381d343a773519ea04bb8c3d72efb7e7b
Stanisław Zakrzewski <stanislaw.zakrzewski13@gmail.com>
2020-05-04 19:18:37 +0200

    dziaua

--- src/data_visualization/graphs.py
+++ src/data_visualization/graphs.py
@@ -44,9 +44,9 @@ def algorithm_comparison_plot(database_id, metric_id, best_results):
 
 def parameter_plot(database_id, metric_id, algorithm_id, parameter_id, column_1_id, column_2_id, data, results):
     df = pd.DataFrame([], columns=[column_1_id, column_2_id, 'classes'])
-
+    print(data[column_1_id].values)
     for index, result in enumerate(results.values):
-        df.loc[index] = [data[column_1_id][index], data[column_2_id][index], result[0]]
+        df.loc[index] = [data[column_1_id].values[index], data[column_2_id].values[index], result[0]]
 
     sns.scatterplot(x=column_1_id, y=column_2_id, hue='classes', data=df)
 
