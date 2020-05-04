d9808a6ab2501e7e8aaaa11add4115c2fcc52269
Stanisław Zakrzewski <stanislaw.zakrzewski13@gmail.com>
2020-05-04 20:14:19 +0200

    das

--- src/evaluation/evaluationMetric/metrics/accuracy.py
+++ src/evaluation/evaluationMetric/metrics/accuracy.py
@@ -5,4 +5,4 @@ class AccuracyMetric:
 
     @staticmethod
     def calculate(y_prediction, data_map):
-        return accuracy_score(data_map["CLASSIFICATION_COLUMN_test"], y_prediction)
+        return accuracy_score(data_map['FEED_COLUMNS'], y_prediction)
--- src/evaluation/evaluationMetric/metrics/recall.py
+++ src/evaluation/evaluationMetric/metrics/recall.py
@@ -5,4 +5,4 @@ class RecallMetric:
 
     @staticmethod
     def calculate(y_prediction, data_map):
-        return recall_score(data_map["CLASSIFICATION_COLUMN_test"], y_prediction)
+        return recall_score(data_map['FEED_COLUMNS'], y_prediction)
