6f175a24c5596023dd83c2b67d5fa162ab9d4650
Hyphen133 <hyphen133@gmail.com>
2020-06-04 20:55:41 +0200

    Sample extraction encapsulated

--- src/gender_recognition/audio/audio_training_examples.py
+++ src/gender_recognition/audio/audio_training_examples.py
@@ -8,9 +8,9 @@ def get_window_indecies(audio_series, window_size):
 def calculate_noise_or_voice_per_interval(window_intervals, sound_intervalas):
     window_sound_pertengage = {}
 
-    for i in range(1, len(window_indecies)):
+    for i in range(1, len(window_intervals)):
         window_start = window_intervals[i - 1]
-        window_end = window_indecies[i]
+        window_end = window_intervals[i]
 
         # Select all matching windows
         # TODO -> move along
@@ -43,26 +43,35 @@ def calculate_noise_or_voice_per_interval(window_intervals, sound_intervalas):
     return window_sound_pertengage
 
 
+class AudioSampleGenerator:
 
-#
-#   Parameters:
-#   - overlap_threshold - since window does not match with detected silence intervals we calculate percentage of sound in window; above threshold we mark as person speaking
-#   - window_size - size of single sample length (time in seconds can be can be calculated as window_size / sampling_rate
-#   - sampling_rate - for audio read from file
-#   - top_db_silence_threshold - move that value in decibel sound is considered as voice no silence
-#
-def generate_window_is_person_map_for_audio(audio_path, overlap_threshold = 0.2, window_size = 2048, sampling_rate =44100, top_db_silence_threshold = 30):
-    audio_series, sampling_rate = librosa.core.load(audio_path, sr=sampling_rate)
-    # http://jamesmontgomery.us/blog/Voice_Recognition_Model.html
-    splitted_series = librosa.effects.split(audio_series, top_db=top_db_silence_threshold)
-    window_indecies = get_window_indecies(audio_series, window_size)
-    window_overlap_percentage = calculate_noise_or_voice_per_interval(window_indecies, splitted_series)
-    window_is_person_map = {window: (window_overlap_percentage[window] > overlap_threshold) for window in
-                            window_overlap_percentage.keys()}
+    #
+    #   Parameters:
+    #   - overlap_threshold - since window does not match with detected silence intervals we calculate percentage of sound in window; above threshold we mark as person speaking
+    #   - window_size - size of single sample length (time in seconds can be can be calculated as window_size / sampling_rate
+    #   - sampling_rate - for audio read from file
+    #   - top_db_silence_threshold - move that value in decibel sound is considered as voice no silence
+    #
+    def __init__(self,overlap_threshold = 0.2, window_size = 2048, sampling_rate =44100, top_db_silence_threshold = 30) -> None:
+        super().__init__()
+        self.overlap_threshold = overlap_threshold
+        self.window_size = window_size
+        self.sampling_rate = sampling_rate
+        self.top_db_silence_threshold = top_db_silence_threshold
 
-    return window_is_person_map
+
+    def generate_window_is_person_map_for_audio(self, audio_path):
+        audio_series, sampling_rate = librosa.core.load(audio_path, sr=self.sampling_rate)
+        # http://jamesmontgomery.us/blog/Voice_Recognition_Model.html
+        splitted_series = librosa.effects.split(audio_series, top_db=self.top_db_silence_threshold)
+        window_indecies = get_window_indecies(audio_series, self.window_size)
+        window_overlap_percentage = calculate_noise_or_voice_per_interval(window_indecies, splitted_series)
+        window_is_person_map = {window: (window_overlap_percentage[window] > self.overlap_threshold) for window in
+                                window_overlap_percentage.keys()}
+
+        return window_is_person_map
 
 if __name__ =='__main__':
-    generate_window_is_person_map_for_audio('k_1_1.wav')
+    window_is_person_map = AudioSampleGenerator().generate_window_is_person_map_for_audio('k_1_1.wav')
 
 
